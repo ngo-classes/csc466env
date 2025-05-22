fddecf11615ef3cc0cdc132e7afd99c0a0dae240
Linh B Ngo <lbn28@drexel.edu>
2025-05-22 10:08:37 -0400

    internal home now

--- docker/head-student/entrypoint.sh
+++ docker/head-student/entrypoint.sh
@@ -3,14 +3,15 @@
 echo "---> Starting sshd on the slurmd..."
 sudo /usr/sbin/sshd -e
 
-if [ -z "$( ls -A '/path/to/dir' )" ]; then
+if [ -z "$( ls -A '/home/student/.ssh' )" ]; then
   echo "Initializing /home/student on first run"
-  cp -R /opt/home/student /home/student  
+  cp -R /opt/home/student /home/  
   chmod 700 /home/student/.ssh
   chmod 600 /home/student/.ssh/id_rsa
   chmod 600 /home/student/.ssh/id_rsa.pub
   chmod 600 /home/student/.ssh/authorized_keys
   chmod 600 /home/student/.ssh/config
+fi
 
 cd /home/student/
 jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 &
